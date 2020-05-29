56264623683a9b9e887a088b4f47bc22673cd622
Daniil <64782611+comictomcat@users.noreply.github.com>
2020-05-29 20:34:47 +0300

    добавил новый фильтр

--- filters/__init__.py
+++ filters/__init__.py
@@ -2,7 +2,10 @@ from aiogram import Dispatcher
 
 from .group_chat import IsGroup
 from .private_chat import IsPrivate
+from .is_banable import IsBanable
+
 
 def setup(dp: Dispatcher):
     dp.filters_factory.bind(IsGroup)
     dp.filters_factory.bind(IsPrivate)
+    dp.filters_factory.bind(IsBanable)
