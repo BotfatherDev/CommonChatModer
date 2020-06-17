1f8c33b41db8d8f4652dcd6f1e8bc16e355c5d3e
Kostiantyn Kriuchkov <36363097+Latand@users.noreply.github.com>
2020-06-17 23:45:14 +0300

    Update is_banable.py

--- filters/is_banable.py
+++ filters/is_banable.py
@@ -3,7 +3,7 @@ from aiogram.dispatcher.filters import BoundFilter
 from loader import bot
 
 
-class IsBanable(BoundFilter):
+class CanBan(BoundFilter):
 
     async def check(self, message: types.Message):
         member = await bot.get_chat_member(message.chat.id, message.from_user.id)
