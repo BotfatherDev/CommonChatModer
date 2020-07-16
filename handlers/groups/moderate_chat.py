1890a512fa641ef064861bd45e60765779aa4bf1
0kit <kostyajog@mail.ru>
2020-07-16 17:43:10 +0300

    has permission filter
    Добавлен фильтр has permission, проверяющий, обладает ли пользователь необходимыми правами
    Добавлен фильтр is reply, проверяющий, является ли сообщение ответом
    Немного подправлена архитектура (теперь фильтры создаются до хендлеров)
    Добавлено логгирование при запуске бота
    В коммандах бота добавлена подсказка admin only

--- handlers/groups/moderate_chat.py
+++ handlers/groups/moderate_chat.py
@@ -8,11 +8,16 @@ from aiogram.utils.exceptions import BadRequest
 from loguru import logger
 
 from data.permissions import user_ro, user_allowed, no_media
-from filters import IsGroup, CanBan
+from filters import IsGroup
 from loader import bot, dp
 
 
-@dp.message_handler(IsGroup(), Command(commands=["ro"], prefixes="!/"), CanBan())
+@dp.message_handler(
+    IsGroup(),
+    regexp=r"(!ro|/ro) ?(\d+)? ?([\w+\D]+)?",
+    is_reply=True,
+    user_can_restrict_members=True,
+)
 async def read_only_mode(message: types.Message):
     """Хендлер с фильтром в группе, где можно использовать команду !ro ИЛИ /ro
     :time int: время на которое нужно замутить пользователя в минутах
@@ -23,16 +28,9 @@ async def read_only_mode(message: types.Message):
     admin_username = message.from_user.username
     admin_mentioned = message.from_user.get_mention(as_html=True)
 
-    # Пробуем присвоить значения пересланного пользователя
-    try:
-        member_id = message.reply_to_message.from_user.id
-        member_username = message.reply_to_message.from_user.username
-        member_mentioned = message.reply_to_message.from_user.get_mention(as_html=True)
-
-    # Ловим ошибку, если пользователь не переслал сообщение
-    except AttributeError:
-        await message.reply("Ошибка. Нужно переслать сообщение")
-        return False
+    member_id = message.reply_to_message.from_user.id
+    member_username = message.reply_to_message.from_user.username
+    member_mentioned = message.reply_to_message.from_user.get_mention(as_html=True)
 
     # Разбиваем команду на аргументы с помощью RegExp
     command_parse = re.compile(r"(!ro|/ro) ?(\d+)? ?([\w+\D]+)?")
@@ -49,7 +47,7 @@ async def read_only_mode(message: types.Message):
     if not reason:
         reason = "без указания причины"
     else:
-        reason = "по причине: " + reason
+        reason = f"по причине: {reason}"
     # Получаем конечную дату, до которой нужно замутить
     until_date = datetime.datetime.now() + datetime.timedelta(minutes=int(time))
 
@@ -59,7 +57,8 @@ async def read_only_mode(message: types.Message):
         await message.chat.restrict(
             user_id=member_id,
             permissions=user_ro,
-            until_date=until_date)
+            until_date=until_date,
+        )
 
         # Отправляем сообщение
         await message.answer(
@@ -78,7 +77,6 @@ async def read_only_mode(message: types.Message):
 
     # Если бот не может замутить пользователя (администратора), возникает ошибка BadRequest которую мы обрабатываем
     except BadRequest:
-
         # Отправляем сообщение
         await message.answer(
             f"Пользователь {member_mentioned} "
@@ -102,7 +100,7 @@ async def read_only_mode(message: types.Message):
         await service_message.delete()
 
 
-@dp.message_handler(IsGroup(), Command(commands=["unro"], prefixes="!/"), CanBan())
+@dp.message_handler(IsGroup(), Command(commands=["unro"], prefixes="!/"), is_reply=True, user_can_restrict_members=True)
 async def undo_read_only_mode(message: types.Message):
     """Хендлер с фильтром в группе, где можно использовать команду !unro ИЛИ /unro"""
 
@@ -110,17 +108,9 @@ async def undo_read_only_mode(message: types.Message):
     admin_username = message.from_user.username
     admin_mentioned = message.from_user.get_mention(as_html=True)
     chat_id = message.chat.id
-
-    # Пробуем присвоить значения пересланного пользователя
-    try:
-        member_id = message.reply_to_message.from_user.id
-        member_username = message.reply_to_message.from_user.username
-        member_mentioned = message.reply_to_message.from_user.get_mention(as_html=True)
-
-    # Ловим ошибку, если пользователь не переслал сообщение
-    except AttributeError:
-        await message.reply("Ошибка. Нужно переслать сообщение")
-        return False
+    member_id = message.reply_to_message.from_user.id
+    member_username = message.reply_to_message.from_user.username
+    member_mentioned = message.reply_to_message.from_user.get_mention(as_html=True)
 
     # Возвращаем пользователю возможность отправлять сообщения
     await bot.restrict_chat_member(
@@ -146,25 +136,16 @@ async def undo_read_only_mode(message: types.Message):
     await service_message.delete()
 
 
-@dp.message_handler(IsGroup(), Command(commands=["ban"], prefixes="!/"), CanBan())
+@dp.message_handler(IsGroup(), Command(commands=["ban"], prefixes="!/"), is_reply=True, user_can_restrict_members=True)
 async def ban_user(message: types.Message):
     """Хендлер с фильтром в группе, где можно использовать команду !ban ИЛИ /ban"""
 
     # Создаем переменные для удобства
     admin_fullname = message.from_user.full_name
     admin_mentioned = message.from_user.get_mention(as_html=True)
-
-    # Пробуем присвоить значения пересланного пользователя
-    try:
-        member_id = message.reply_to_message.from_user.id
-        member_fullname = message.reply_to_message.from_user.full_name
-        member_mentioned = message.reply_to_message.from_user.get_mention(as_html=True)
-
-    # Ловим ошибку, если пользователь не переслал сообщение
-    except AttributeError:
-        await message.reply("Ошибка. Нужно переслать сообщение")
-        return False
-
+    member_id = message.reply_to_message.from_user.id
+    member_fullname = message.reply_to_message.from_user.full_name
+    member_mentioned = message.reply_to_message.from_user.get_mention(as_html=True)
     try:
         # Пытаемся удалить пользователя из чата
         await message.chat.kick(user_id=member_id)
@@ -211,24 +192,16 @@ async def ban_user(message: types.Message):
         await service_message.delete()
 
 
-@dp.message_handler(IsGroup(), Command(commands=["unban"], prefixes="!/"), CanBan())
+@dp.message_handler(IsGroup(), Command(commands=["unban"], prefixes="!/"), is_reply=True, user_can_restrict_members=True)
 async def unban_user(message: types.Message):
     """Хендлер с фильтром в группе, где можно использовать команду !unban ИЛИ /unban"""
 
     # Создаем переменные для удобства
     admin_username = message.from_user.username
     admin_mentioned = message.from_user.get_mention(as_html=True)
-
-    # Пробуем присвоить значения пересланного пользователя
-    try:
-        member_id = message.reply_to_message.from_user.id
-        member_username = message.reply_to_message.from_user.username
-        member_mentioned = message.reply_to_message.from_user.get_mention(as_html=True)
-
-    # Ловим ошибку, если пользователь не переслал сообщение
-    except AttributeError:
-        await message.reply("Ошибка. Нужно переслать сообщение")
-        return False
+    member_id = message.reply_to_message.from_user.id
+    member_username = message.reply_to_message.from_user.username
+    member_mentioned = message.reply_to_message.from_user.get_mention(as_html=True)
 
     # И разбаниваем
     await message.chat.unban(user_id=member_id)
@@ -250,21 +223,14 @@ async def unban_user(message: types.Message):
     await service_message.delete()
 
 
-@dp.message_handler(IsGroup(), Command(commands=["media_false"], prefixes="!/"), CanBan())
+@dp.message_handler(IsGroup(), Command(commands=["media_false"], prefixes="!/"), is_reply=True, user_can_restrict_members=True)
 async def media_false_handler(message: types.Message):
     admin_username = message.from_user.username
     admin_mentioned = message.from_user.get_mention(as_html=True)
+    member_id = message.reply_to_message.from_user.id
+    member_username = message.reply_to_message.from_user.username
+    member_mentioned = message.reply_to_message.from_user.get_mention(as_html=True)
 
-    # Пробуем присвоить значения пересланного пользователя
-    try:
-        member_id = message.reply_to_message.from_user.id
-        member_username = message.reply_to_message.from_user.username
-        member_mentioned = message.reply_to_message.from_user.get_mention(as_html=True)
-
-    # Ловим ошибку, если пользователь не переслал сообщение
-    except AttributeError:
-        await message.reply("Ошибка. Нужно переслать сообщение")
-        return
     command_parse = re.compile(r"(!media_false|/media_false) ?(\d+)?")
     parsed = command_parse.match(message.text)
     time = parsed.group(2)
@@ -294,7 +260,8 @@ async def media_false_handler(message: types.Message):
 
         # Вносим информацию о муте в лог
         logger.info(
-            f"Пользователю @{member_username} запрещено использовать медиаконтент до {until_date} админом @{admin_username}"
+            f"Пользователю @{member_username} запрещено использовать медиаконтент до {until_date} "
+            f"админом @{admin_username}"
         )
 
         service_message = await message.reply("Сообщение самоуничтожится через 5 секунд")
@@ -324,22 +291,14 @@ async def media_false_handler(message: types.Message):
         await service_message.delete()
 
 
-@dp.message_handler(IsGroup(), Command(commands=["media_true"], prefixes="!/"), CanBan())
+@dp.message_handler(IsGroup(), Command(commands=["media_true"], prefixes="!/"), is_reply=True, user_can_restrict_members=True)
 async def media_true_handler(message: types.Message):
     admin_username = message.from_user.username
     admin_mentioned = message.from_user.get_mention(as_html=True)
     chat_id = message.chat.id
-
-    # Пробуем присвоить значения пересланного пользователя
-    try:
-        member_id = message.reply_to_message.from_user.id
-        member_username = message.reply_to_message.from_user.username
-        member_mentioned = message.reply_to_message.from_user.get_mention(as_html=True)
-
-    # Ловим ошибку, если пользователь не переслал сообщение
-    except AttributeError:
-        await message.reply("Ошибка. Нужно переслать сообщение")
-        return
+    member_id = message.reply_to_message.from_user.id
+    member_username = message.reply_to_message.from_user.username
+    member_mentioned = message.reply_to_message.from_user.get_mention(as_html=True)
 
     try:
         # Возвращаем пользователю возможность отправлять медиаконтент
@@ -350,7 +309,8 @@ async def media_true_handler(message: types.Message):
         )
 
         # Информируем об этом
-        await message.answer(f"Пользователь {member_mentioned} благодаря {admin_mentioned} может снова использовать медиаконтент")
+        await message.answer(f"Пользователь {member_mentioned} "
+                             f"благодаря {admin_mentioned} может снова использовать медиаконтент")
         logger.info(
             f"Пользователь @{member_username} благодаря @{admin_username} может снова использовать медиаконтент"
         )
