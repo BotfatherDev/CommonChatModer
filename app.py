6d087657aadc9afb78e699b9268a694f28fb26fe
Kostiantyn Kriuchkov <latand666@gmail.com>
2020-08-22 00:04:29 +0300

    fix

--- app.py
+++ app.py
@@ -7,14 +7,13 @@ from loader import db, dp
 from utils.notify_admins import on_startup_notify
 from utils.set_bot_commands import set_default_commands
 import filters
-import middlewares
 
 
 async def on_startup(dp):
-    import handlers
-
     filters.setup(dp)
-    logger.info("Подключение handlers...")
+
+    import handlers
+    import middlewares
     middlewares.setup(dp)
 
     await set_default_commands(dp)
@@ -24,6 +23,7 @@ async def on_startup(dp):
     except Exception as err:
         print(err)
     logger.info("Бот запущен")
+
 
 if __name__ == '__main__':
     executor.start_polling(dp, on_startup=on_startup, skip_updates=SKIP_UPDATES)
