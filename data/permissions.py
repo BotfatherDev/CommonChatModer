7a745adf99a3f25b03cbae1ee4d4e98d07c05bde
Логгин <123play123.loggin@gmail.com>
2021-03-19 19:34:10 +0700

    Casino с призами

--- data/permissions.py
+++ data/permissions.py
@@ -35,7 +35,6 @@ def set_new_user_approved_permissions():
 # Права пользователя в муте
 def set_user_ro_permissions():
     new_permissions = copy(default_permissions)
-    print(new_permissions)
     new_permissions.update(
         can_send_messages=False,
         can_send_media_messages=False,
@@ -44,7 +43,6 @@ def set_user_ro_permissions():
         can_add_web_page_previews=False,
         can_invite_users=False
     )
-    print(new_permissions)
     return types.ChatPermissions(
         **new_permissions
     )
