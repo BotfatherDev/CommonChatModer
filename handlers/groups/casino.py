92ab1ae9e57a44a31fc8eeadee460848a809d19f
kuttLoggin <123play123.loggin@gmail.com>
2021-03-21 03:15:57 +0700

    Update casino.py
    Чуть синхронности с анимацией

--- handlers/groups/casino.py
+++ handlers/groups/casino.py
@@ -47,7 +47,7 @@ async def win_or_loss(message: types.Message):
             name = message.from_user.get_mention(as_html=True)
 
             try:
-                await asyncio.sleep(1.3)
+                await asyncio.sleep(1.5)
                 await message.chat.restrict(
                     user_id=message.from_user.id,
                     permissions=set_user_ro_permissions(),
