e1b39e34f266c443154c23b0951b30ba1d6f8c53
latand <latand666@gmail.com>
2021-10-03 09:31:53 +0300

    Убрал пересылку сообщения админу

--- handlers/groups/report.py
+++ handlers/groups/report.py
@@ -1,6 +1,7 @@
 from aiogram import types
 from aiogram.dispatcher.filters import Command
 from aiogram.utils.markdown import hlink
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
 from data.config import ADMINS_ID
 from filters import IsGroup, IsReplyFilter
@@ -9,6 +10,7 @@ from utils.misc.display_name import get_display_name
 
 report_command = Command("report", prefixes={"/", "!"})
 
+AsyncIOScheduler
 
 @dp.message_handler(IsGroup(), IsReplyFilter(True), report_command)
 async def report_user(message: types.Message):
@@ -25,11 +27,6 @@ async def report_user(message: types.Message):
             admin_id,
             f"Кинут репорт на пользователя {display_name} "
             "за следующее " + hlink("сообщение", message.reply_to_message.url)
-        )
-        await bot.forward_message(
-            admin_id,
-            from_chat_id=message.chat.id,
-            message_id=message.reply_to_message.message_id
         )
 
 
