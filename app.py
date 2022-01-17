dd51714135b493b7e69ce42bfff86f60ce23e57e
Kostiantyn Kriuchkov <36363097+Latand@users.noreply.github.com>
2022-01-17 14:50:27 +0200

    no alembic migration #2

--- app.py
+++ app.py
@@ -20,7 +20,6 @@ async def on_startup(dp):
     await set_default_commands(dp)
     await on_startup_notify(dp)
     
-    db.drop_table('RatingUsers')
     try:
         db.create_table_stickers()
         db.create_table_chat_admins()
