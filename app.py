62d0715c668fca0a548094142627d044d1f74523
latand <latand666@gmail.com>
2022-04-04 11:40:45 +0300

    rollback not intended changes

--- app.py
+++ app.py
@@ -9,15 +9,6 @@ from loader import db, dp
 from utils.notify_admins import on_startup_notify
 from utils.set_bot_commands import set_default_commands
 
-
-from aiohttp import web
-
-from data import config
-from data.config import WEBHOOK_URL, WEBHOOK_PATH, WEBAPP_HOST, WEBAPP_PORT, ip
-from loader import SSL_CERTIFICATE, ssl_context, bot
-from utils.set_bot_commands import set_default_commands
-from webserver.handler import app, dp
-
 middlewares.setup(dp)
 filters.setup(dp)
 
@@ -46,26 +37,3 @@ if __name__ == "__main__":
         skip_updates=SKIP_UPDATES,
         allowed_updates=types.AllowedUpdates.all()
     )
-
-
-
-async def on_startup(app):
-    await bot.set_webhook(
-        url=WEBHOOK_URL,
-        certificate=SSL_CERTIFICATE
-    )
-
-    webhook = await bot.get_webhook_info()
-    import filters
-    import middlewares
-
-    filters.setup(dp)
-    middlewares.setup(dp)
-
-    from utils.notify_admins import on_startup_notify
-    await on_startup_notify(dp)
-    await set_default_commands(dp)
-
-
-app.on_startup.append(on_startup)
-web.run_app(app, host="0.0.0.0", port=config.WEBHOOK_PORT, ssl_context=ssl_context)
