2a2a317b832d7737f54f7e2dbb5786b109e298f3
latand <latand666@gmail.com>
2022-04-04 14:12:57 +0300

    improved reporting function

--- handlers/groups/report.py
+++ handlers/groups/report.py
@@ -6,6 +6,8 @@ from aiogram.utils.markdown import hlink
 
 from filters import IsGroup, IsReplyFilter
 from loguru import logger
+
+from keyboards.inline.admin import report_reactions_keyboard, report_cb
 from loader import dp, db
 
 report_command = Command("report", prefixes={"/", "!"})
@@ -61,7 +63,11 @@ async def report_user(message: types.Message):
             await dp.bot.send_message(
                 chat_id=admin_id,
                 text=f"Кинут репорт на пользователя {mention} "
-                     "за следующее " + hlink("сообщение", message.reply_to_message.url)
+                     "за следующее " + hlink("сообщение", message.reply_to_message.url),
+                reply_markup=report_reactions_keyboard(
+                    message.reply_to_message.chat.id,
+                    message.reply_to_message.from_user.id,
+                    message.reply_to_message.message_id)
             )
             await asyncio.sleep(0.05)
         except (exceptions.BotBlocked, exceptions.UserDeactivated, exceptions.CantTalkWithBots,
@@ -79,3 +85,25 @@ async def report_user_if_command_is_not_reply(message: types.Message):
         "Сообщение с командой должно быть ответом на сообщение пользователя, "
         "на которого вы хотите пожаловаться"
     )
+
+
+@dp.callback_query_handler(report_cb.filter())
+async def report_user_callback(call: types.CallbackQuery, callback_data: dict):
+    action = callback_data.get('action')
+    message_id = callback_data.get('message_id')
+    user_id = callback_data.get('user_id')
+    chat_id = callback_data.get('chat_id')
+    try:
+        if action == 'ban':
+            await call.bot.kick_chat_member(
+                chat_id, user_id, revoke_messages=False
+            )
+            await call.bot.delete_message(chat_id, message_id)
+        elif action == 'ban_delete':
+            await call.bot.kick_chat_member(
+                chat_id, user_id, revoke_messages=True
+            )
+        elif action == 'delete':
+            await call.bot.delete_message(chat_id, message_id)
+    finally:
+        await call.message.delete_reply_markup()
